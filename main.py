1b2028e97434735dadff82879c0f3f27d018b47d
hahg2000 <983759734@qq.com>
2026-02-22 18:34:13 +0800

    feat:用策略模式重构代码，使后续更容易新增钓鱼方案

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ import pydirectinput
 import time
 import ctypes
 import utils
+import qte_strategy as strategy
 ctypes.windll.user32.SetProcessDPIAware()
 GAME_TITLE = "BrownDust II" 
 
 region = utils.get_window_region(GAME_TITLE)
 config = utils.read_ini()
 
-# ================================== 读取配置 ==================================
-longest_keep_time = utils.readConfigAndCastInt(config, "time", "longest_keep_time")
+QTE_STRATEGIES_MAP = {
+    "烟波湖": strategy.FrostStraitQTEStrategy,
+    "浅岸": strategy.FrostStraitQTEStrategy,
+    "寒霜海峡": strategy.FrostStraitQTEStrategy,
+    "深渊巨口": strategy.AbyssMawQTEStrategy,
+    "亚特兰蒂斯": strategy.FrostStraitQTEStrategy,
+}
 
-roi_top_percent = utils.readConfigAndCastInt(config, "roi", "top_percent")
-roi_bottom_percent = utils.readConfigAndCastInt(config, "roi", "bottom_percent")
-roi_left_percent = utils.readConfigAndCastInt(config, "roi", "left_percent")
-roi_right_percent = utils.readConfigAndCastInt(config, "roi", "right_percent")
+# ================================== 读取配置 ==================================
 
 hook_top_percent = utils.readConfigAndCastInt(config, "hook", "top_percent")
 hook_bottom_percent = utils.readConfigAndCastInt(config, "hook", "bottom_percent")
@@ -31,31 +34,6 @@ hook_upper_yellow_hue = utils.readConfigAndCastInt(config, 'hook', 'hook_upper_y
 hook_upper_yellow_saturation = utils.readConfigAndCastInt(config, 'hook', 'hook_upper_yellow_saturation')
 hook_upper_yellow_value = utils.readConfigAndCastInt(config, 'hook', 'hook_upper_yellow_value')
 
-lower_white_hue = utils.readConfigAndCastInt(config, 'roi', 'lower_white_hue')
-lower_white_saturation = utils.readConfigAndCastInt(config, 'roi', 'lower_white_saturation')
-lower_white_value = utils.readConfigAndCastInt(config, 'roi', 'lower_white_value')
-upper_white_hue = utils.readConfigAndCastInt(config, 'roi', 'upper_white_hue')
-upper_white_saturation = utils.readConfigAndCastInt(config, 'roi', 'upper_white_saturation')
-upper_white_value = utils.readConfigAndCastInt(config, 'roi', 'upper_white_value')
-lower_white = np.array([lower_white_hue, lower_white_saturation, lower_white_value]) 
-upper_white = np.array([upper_white_hue, upper_white_saturation, upper_white_value])
-
-lower_red_hue = utils.readConfigAndCastInt(config, "roi", "lower_red_hue")
-lower_red_saturation = utils.readConfigAndCastInt(config, "roi", "lower_red_saturation")
-lower_red_value = utils.readConfigAndCastInt(config, "roi", "lower_red_value")
-upper_red_hue = utils.readConfigAndCastInt(config, "roi", "upper_red_hue")
-upper_red_saturation = utils.readConfigAndCastInt(config, "roi", "upper_red_saturation")
-upper_red_value = utils.readConfigAndCastInt(config, "roi", "upper_red_value")
-lower_red = np.array([lower_red_hue, lower_red_saturation, lower_red_value])
-upper_red = np.array([upper_red_hue, upper_red_saturation, upper_red_value])
-
-lower_yellow_hue = utils.readConfigAndCastInt(config, "roi", "lower_yellow_hue")
-lower_yellow_saturation = utils.readConfigAndCastInt(config, "roi", "lower_yellow_saturation")
-lower_yellow_value = utils.readConfigAndCastInt(config, "roi", "lower_yellow_value")
-upper_yellow_hue = utils.readConfigAndCastInt(config, "roi", "upper_yellow_hue")
-upper_yellow_saturation = utils.readConfigAndCastInt(config, "roi", "upper_yellow_saturation")
-upper_yellow_value = utils.readConfigAndCastInt(config, "roi", "upper_yellow_value")
-
 lower_green_hue = utils.readConfigAndCastInt(config, "roi", "lower_green_hue")
 lower_green_saturation = utils.readConfigAndCastInt(config, "roi", "lower_green_saturation")
 lower_green_value = utils.readConfigAndCastInt(config, "roi", "lower_green_value")
@@ -64,38 +42,6 @@ upper_green_saturation = utils.readConfigAndCastInt(config, "roi", "upper_green_
 upper_green_value = utils.readConfigAndCastInt(config, "roi", "upper_green_value")
 lower_green = np.array([lower_green_hue, lower_green_saturation, lower_green_value])
 upper_green = np.array([upper_green_hue, upper_green_saturation, upper_green_value])
-
-lower_blue_hue = utils.readConfigAndCastInt(config, "roi", "lower_blue_hue")
-lower_blue_saturation = utils.readConfigAndCastInt(config, "roi", "lower_blue_saturation")
-lower_blue_value = utils.readConfigAndCastInt(config, "roi", "lower_blue_value")
-upper_blue_hue = utils.readConfigAndCastInt(config, "roi", "upper_blue_hue")
-upper_blue_saturation = utils.readConfigAndCastInt(config, "roi", "upper_blue_saturation")
-upper_blue_value = utils.readConfigAndCastInt(config, "roi", "upper_blue_value")
-lower_blue = np.array([lower_blue_hue, lower_blue_saturation, lower_blue_value])
-upper_blue = np.array([upper_blue_hue, upper_blue_saturation, upper_blue_value])
-
-time_lower_green_hue = utils.readConfigAndCastInt(config, 'roi', 'time_lower_green_hue')
-time_lower_green_saturation = utils.readConfigAndCastInt(config, 'roi', 'time_lower_green_saturation')
-time_lower_green_value = utils.readConfigAndCastInt(config, 'roi', 'time_lower_green_value')
-time_upper_green_hue = utils.readConfigAndCastInt(config, 'roi', 'time_upper_green_hue')
-time_upper_green_saturation = utils.readConfigAndCastInt(config, 'roi', 'time_upper_green_saturation')
-time_upper_green_value = utils.readConfigAndCastInt(config, 'roi', 'time_upper_green_value')
-time_lower_green = np.array([time_lower_green_hue, time_lower_green_saturation, time_lower_green_value])
-time_upper_green = np.array([time_upper_green_hue, time_upper_green_saturation, time_upper_green_value])
-
-time_lower_red_hue = utils.readConfigAndCastInt(config, 'roi', 'time_lower_red_hue')
-time_lower_red_saturation = utils.readConfigAndCastInt(config, 'roi', 'time_lower_red_saturation')
-time_lower_red_value = utils.readConfigAndCastInt(config, 'roi', 'time_lower_red_value')
-time_upper_red_hue = utils.readConfigAndCastInt(config, 'roi', 'time_upper_red_hue')
-time_upper_red_saturation = utils.readConfigAndCastInt(config, 'roi', 'time_upper_red_saturation')
-time_upper_red_value = utils.readConfigAndCastInt(config, 'roi', 'time_upper_red_value')
-time_lower_red = np.array([time_lower_red_hue, time_lower_red_saturation, time_lower_red_value])
-time_upper_red = np.array([time_upper_red_hue, time_upper_red_saturation, time_upper_red_value])
-
-time_top_percent = utils.readConfigAndCastInt(config, 'roi', 'time_top_percent')
-time_bottom_percent = utils.readConfigAndCastInt(config, 'roi', 'time_bottom_percent')
-time_left_percent = utils.readConfigAndCastInt(config, 'roi', 'time_left_percent')
-time_right_percent = utils.readConfigAndCastInt(config, 'roi', 'time_right_percent')
 
 if not region:
     input(">>> 程序结束，按回车键关闭")
@@ -106,20 +52,6 @@ hook_pos = {
     "top": region["top"] + int(region["height"] * hook_top_percent / 100),
     "width": int(region["width"] * (hook_right_percent - hook_left_percent) / 100),
     "height": int(region["height"] * (hook_bottom_percent - hook_top_percent) / 100)
-}
-
-roi_pos = {
-    "left": region["left"] + int(region["width"] * roi_left_percent / 100),
-    "top": region["top"] + int(region["height"] * roi_top_percent / 100),
-    "width": int(region["width"] * (roi_right_percent - roi_left_percent) / 100),
-    "height": int(region["height"] * (roi_bottom_percent - roi_top_percent) / 100)
-}
-
-rot_pos = {
-    "left": region["left"] + int(region["width"] * time_left_percent / 100),
-    "top": region["top"] + int(region["height"] * time_top_percent / 100),
-    "width": int(region["width"] * (time_right_percent - time_left_percent) / 100),
-    "height": int(region["height"] * (time_bottom_percent - time_top_percent) / 100)
 }
 
 # ================================== 功能区 ==================================
@@ -171,91 +103,15 @@ def wait_for_bite(sct):
             pydirectinput.keyUp("up")
 
             # 点击屏幕
-            finish_fishing(region["left"] + region["width"]//2, region["top"] + region["height"]//2)
+            # 移动鼠标到窗口中心 (防止点歪)
+            pydirectinput.moveTo(region["left"] + region["width"]//2, region["top"] + region["height"]//2)
+            time.sleep(0.2)
+            # 点击左键
+            pydirectinput.click()
 
             # 重新抛杆
             cast_rod()
-            continue
- 
-def play_qte(sct):
-    print(">>> 开始 QTE...")
-
-    no_bar_frames = 0
-    start_time = time.time()
-    while time.time() - start_time < longest_keep_time:
-        # 提取qte条的范围
-        roi = np.array(sct.grab(roi_pos))
-        rot = np.array(sct.grab(rot_pos))
-        
-        # MSS 默认是 BGRA
-        roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGRA2BGR) 
-        roi_hsv = cv2.cvtColor(roi_hsv, cv2.COLOR_BGR2HSV)
-        rot_hsv = cv2.cvtColor(rot, cv2.COLOR_BGRA2BGR)
-        rot_hsv = cv2.cvtColor(rot_hsv, cv2.COLOR_BGR2HSV)
-        
-        # 提取黄色区域
-        mask_yellow = utils.create_color_mask([lower_yellow_hue, lower_yellow_saturation, lower_yellow_value], 
-                                              [upper_yellow_hue, upper_yellow_saturation, upper_yellow_value], 
-                                              roi_hsv)
-        
-        # 提取时间条的绿色和红色区域
-        mask_green_time = utils.create_color_mask([time_lower_green_hue, time_lower_green_saturation, time_lower_green_value], 
-                                                  [time_upper_green_hue, time_upper_green_saturation, time_upper_green_value], 
-                                                  rot_hsv, is_dilate=False)
-        mask_red_time = utils.create_color_mask([time_lower_red_hue, time_lower_red_saturation, time_lower_red_value], 
-                                                [time_upper_red_hue, time_upper_red_saturation, time_upper_red_value], 
-                                                rot_hsv, is_dilate=False)
-        time_red_pixel_count = cv2.countNonZero(mask_red_time) * 20
-        time_green_pixel_count = cv2.countNonZero(mask_green_time) * 10
-        
-        # 防止有其他有元素干扰
-        if time_red_pixel_count + time_green_pixel_count > 1000: 
-            no_bar_frames = 0
-            # 找到光标位置
-            mask_cursor = utils.create_color_mask(lower_white, upper_white, roi_hsv, is_dilate=False)
-            col_sums = np.sum(mask_cursor, axis=0)
-            # 找到白色像素最多的那一列的索引
-            cursor_x = np.argmax(col_sums)
-            
-            #  如果画面里没有白色，cursor_x 可能是 0，需要防呆
-            if np.max(col_sums) > 0: 
-                check_y = roi.shape[0] // 2
-                if mask_yellow[check_y, cursor_x]:
-                    print(">>> 击中了黄色区域")
-                    pydirectinput.press("space")
-        else:
-            no_bar_frames += 1
-
-            if no_bar_frames > 30:
-                print(">>> 钓鱼结束")
-                time.sleep(float(config["time"]["fish_end_wait_time"]))
-                finish_fishing(region["left"] + region["width"]//2, region["top"] + region["height"]//2)
-                break 
-
-def finish_fishing(window_center_x, window_center_y):
-    # 移动鼠标到窗口中心 (防止点歪)
-    pydirectinput.moveTo(window_center_x, window_center_y)
-    time.sleep(0.2)
-    # 点击左键
-    pydirectinput.click()
-
-def solve_ice_trouble(roi_hsv):
-    """
-    检测并解决冰冻状态
-    :param roi_hsv: 当前QTE画面的 HSV 数据
-    :return: True 如果检测到冰冻并执行了操作，False 如果无冰冻
-    """
-    mask = cv2.inRange(roi_hsv, lower_red, upper_red)
-                
-    # 计算红色像素数量，阈值设为 10 左右
-    if cv2.countNonZero(mask) > 10: 
-        print(">>> 检测到冰冻！正在破冰 (按空格)...")
-        pydirectinput.press('space')
-        # 为了防止按太快游戏不识别，给个极短的间隔
-        time.sleep(0.05) 
-        return True
-    
-    return False
+            continue   
     
 def clear_backpack():
     print(">>> 清理背包")
@@ -298,26 +154,37 @@ def clear_backpack():
     time.sleep(0.5)
 
 def main():
-    time.sleep(float(config["time"]["begin_fish_wait_time"]))
-    try:
-        with mss.mss() as sct: 
-            while True:
-                # 1. 抛竿
-                cast_rod()
-                
-                # 2. 等待上钩
-                wait_for_bite(sct)
-                
-                # 3. QTE
-                play_qte(sct) 
-                
-                # 4. 结束
-                print("================这轮的钓鱼结束================")
-                time.sleep(float(config["time"]["round_end_wait_time"]))
-    except BaseException:
-        input(">>> 程序结束，按回车键关闭")
-
+    print("可选钓鱼地点：")
+    for idx, location in enumerate(QTE_STRATEGIES_MAP.keys()):
+        print(f"{idx + 1}: {location}")
+    selected_location = input(">>> 输入数字对应的钓鱼地点：")
+    
+    if (int(selected_location) - 1) in range(len(QTE_STRATEGIES_MAP)):
+        selected_location_name = list(QTE_STRATEGIES_MAP.keys())[int(selected_location) - 1]
+        print(f">>> 你选择了: {selected_location_name}")
+        QTEStrategyClass = QTE_STRATEGIES_MAP[selected_location_name]
+    else:
+        print(">>> 选择无效，默认使用寒霜海峡策略")
+        QTEStrategyClass = strategy.FrostStraitQTEStrategy
         
+    qte_strategy = QTEStrategyClass(config, region)
+    
+    time.sleep(float(config["time"]["begin_fish_wait_time"]))
+
+    with mss.mss() as sct: 
+        while True:
+            # 1. 抛竿
+            cast_rod()
+            
+            # 2. 等待上钩
+            wait_for_bite(sct)
+            
+            # 3. QTE
+            qte_strategy.play_qte(sct, region) 
+            
+            # 4. 结束
+            print("================这轮的钓鱼结束================")
+            time.sleep(float(config["time"]["round_end_wait_time"]))      
 
 if __name__ == "__main__": 
     main()
